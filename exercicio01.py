52ac3cfd046e6d253648200e901dd9f59a055043
Denisson César <denissoncesar6@gmail.com>
2025-08-11 14:43:24 -0300

    solução concluída do exercicio01

--- exercicio01.py
+++ exercicio01.py
@@ -4,7 +4,15 @@
 def imprimir_lista(lista):
     for elemento in lista:
         print(elemento)
-    
-numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
 
-imprimir_lista(numeros)
+def main():
+    entrada = input("Digite os números separados por espaço: ")
+    lista_numeros = [int(num) for num in entrada.split()]
+
+    imprimir_lista(lista_numeros)
+
+main()
+    
+#numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+#imprimir_lista(numeros)
